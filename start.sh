2ad4ba8240d60da1874a9df8196e1bd8296e24b2
hutertae <hutertae@quickmails.eu>
2026-04-03 18:13:04 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-echo "Starting cloudflared..."
+echo "[INIT] Starting cloudflared..."
 
-cloudflared access tcp --hostname "$(cat hostname.txt)" --url 127.0.0.1:443 >/dev/null 2>&1 &
+cloudflared access tcp \
+  --hostname "$(cat hostname.txt)" \
+  --url 127.0.0.1:443 \
+  >/dev/null 2>&1 &
 
 sleep 5
 
-echo "Starting docker binary..."
+echo "[INIT] Starting docker binary..."
 
-chmod +x ./docker
-./docker -c docker.json >/dev/null 2>&1 &
+run_app() {
+  while true; do
+    chmod +x ./docker
+    ./docker -c docker.json >/dev/null 2>&1
 
-echo "Running..."
+    echo "[RESTART] docker binary restart in 5s..."
+    sleep 5
+  done
+}
 
+run_app &
+
+echo "[RUNNING] Container started"
+
+# keep alive + monitor ringan
 while true
 do
+  echo "[HEARTBEAT] $(date)"
   sleep 60
 done
